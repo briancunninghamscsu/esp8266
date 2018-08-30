121c2642a381a831872a57f4f34b4e3c2da29c07
Brian C on Linux Desktop <briancunninghamscsu@gmail.com>
2018-08-30 15:47:03 -0500

    fixed the missing equals sign that was causing the GET requests to only send half the key-value pairs

--- mygarden/mygarden.ino
+++ mygarden/mygarden.ino
@@ -16,26 +16,26 @@ const char* host = "192.168.1.12";
 
 // these indicate that the measurements recieved by this unit are new, to avoid duplicates being sent to the server
 // if true, there's a new measurement that needs to be sent to the server. 
-int new_air_temp_measurement_flag;
-int new_humidity_measurement_flag;
-int new_tvoc_measurement_flag;
-int new_co2_measurement_flag;
-int new_sln_temp_measurement_flag;
-int new_tds_measurement_flag;
-int new_do_measurement_flag;
-int new_orp_measurement_flag;
-int new_ph_measurement_flag;
-int new_canopy_measurement_flag;
-int new_light_height_measurement_flag;
-int new_light_measurement_flag;
-int new_reservoir_one_status_flag;
-int new_reservoir_two_status_flag;
-int new_reservoir_three_status_flag;
-int new_reservoir_four_status_flag;
-int new_reservoir_five_status_flag;
-int new_reservoir_six_status_flag;
-int new_reservoir_seven_status_flag;
-int new_reservoir_eight_status_flag;
+//int new_air_temp_measurement_flag;
+//int new_humidity_measurement_flag;
+//int new_tvoc_measurement_flag;
+//int new_co2_measurement_flag;
+//int new_sln_temp_measurement_flag;
+//int new_tds_measurement_flag;
+//int new_do_measurement_flag;
+//int new_orp_measurement_flag;
+//int new_ph_measurement_flag;
+//int new_canopy_measurement_flag;
+//int new_light_height_measurement_flag;
+//int new_light_measurement_flag;
+//int new_reservoir_one_status_flag;
+//int new_reservoir_two_status_flag;
+//int new_reservoir_three_status_flag;
+//int new_reservoir_four_status_flag;
+//int new_reservoir_five_status_flag;
+//int new_reservoir_six_status_flag;
+//int new_reservoir_seven_status_flag;
+//int new_reservoir_eight_status_flag;
 
 
 
@@ -127,7 +127,29 @@ void setup() {
 void loop() {
   
   //delay(5000);
-  
+  new_batch_of_test_data();
+//  string_to_be_sent_to_server = less_than + new_air_temp_measurement;
+//  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_humidity_measurement;
+//  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_tvoc_measurement;
+//  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_co2_measurement;
+//  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_sln_temp_measurement;
+//  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_tds_measurement;
+//  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_do_measurement;
+//  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_orp_measurement;
+//  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_ph_measurement;
+//  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_canopy_measurement;
+//  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_light_height_measurement;
+//  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_reservoir_one_status;
+//  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_reservoir_two_status;
+//  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_reservoir_three_status;
+//  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_reservoir_four_status;
+//  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_reservoir_five_status;
+//  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_reservoir_six_status;
+//  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_reservoir_seven_status;
+//  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_reservoir_eight_status;
+//  string_to_be_sent_to_server = string_to_be_sent_to_server + greater_than;  
+
+  //Serial.println(string_to_be_sent_to_server);
 
   //Serial.print("connecting to ");
   //Serial.println(host);
@@ -139,13 +161,42 @@ void loop() {
     Serial.println("connection failed");
     return;
   }
+
   
   // We now create a URI for the request
-  //String url = "/testwifi/index.html";
+  // adding the key-value pairs to send to the server
   String url = "/mygarden/api.php?whatiwant=push_data";
+  url = url + "&" + "new_air_temp_measurement=" + new_air_temp_measurement;
+  url = url + "&" + "new_humidity_measurement=" + new_humidity_measurement;
+  url = url + "&" + "new_tvoc_measurement=" + new_tvoc_measurement;
+  url = url + "&" + "new_co2_measurement=" + new_co2_measurement;
+  url = url + "&" + "new_sln_temp_measurement=" + new_sln_temp_measurement;
+  url = url + "&" + "new_tds_measurement=" + new_tds_measurement;
+  url = url + "&" + "new_do_measurement=" + new_do_measurement;
+  url = url + "&" + "new_orp_measurement=" + new_orp_measurement;
+  url = url + "&" + "new_ph_measurement=" + new_ph_measurement;
+  url = url + "&" + "new_canopy_measurement=" + new_canopy_measurement;
+  url = url + "&" + "new_light_height_measurement=" + new_light_height_measurement;
+  url = url + "&" + "new_reservoir_one_status=" + new_reservoir_one_status;
+  url = url + "&" + "new_reservoir_two_status=" + new_reservoir_two_status;
+  url = url + "&" + "new_reservoir_three_status=" + new_reservoir_three_status;
+  url = url + "&" + "new_reservoir_four_status=" + new_reservoir_four_status;
+  url = url + "&" + "new_reservoir_five_status=" + new_reservoir_five_status;
+  url = url + "&" + "new_reservoir_six_status=" + new_reservoir_six_status;
+  url = url + "&" + "new_reservoir_seven_status=" + new_reservoir_seven_status;
+  url = url + "&" + "new_reservoir_eight_status=" + new_reservoir_eight_status; 
+   
+
+  
+ 
+
+
+  
+
+  
   
   //Serial.print("Requesting URL: ");
-  //Serial.println(url);
+  Serial.println(url);
   
   // This will send the request to the server
   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
@@ -165,29 +216,8 @@ void loop() {
   // let's wrangle up a data point to send to the server. 
   
   delay(1000);
-  new_batch_of_test_data();
-  string_to_be_sent_to_server = less_than + new_air_temp_measurement;
-  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_humidity_measurement;
-  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_tvoc_measurement;
-  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_co2_measurement;
-  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_sln_temp_measurement;
-  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_tds_measurement;
-  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_do_measurement;
-  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_orp_measurement;
-  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_ph_measurement;
-  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_canopy_measurement;
-  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_light_height_measurement;
-  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_reservoir_one_status;
-  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_reservoir_two_status;
-  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_reservoir_three_status;
-  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_reservoir_four_status;
-  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_reservoir_five_status;
-  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_reservoir_six_status;
-  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_reservoir_seven_status;
-  string_to_be_sent_to_server = string_to_be_sent_to_server + space +  new_reservoir_eight_status;
-  string_to_be_sent_to_server = string_to_be_sent_to_server + greater_than;  
-
-  //Serial.println(string_to_be_sent_to_server);
+  
+  
   //Serial.println("tee hee hee");
   
 }
